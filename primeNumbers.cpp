c948fa5b304f8a3f98f9477114ecbbeb137b62ce
Nikolai <nikolmo@stud.ntnu.no>
2018-04-24 08:48:32 +0200

    Starts counting at 2

--- primeNumbers.cpp
+++ primeNumbers.cpp
@@ -8,7 +8,7 @@ int main() {
 
 	cout << "Prime numbers less than 100\n";
 
-	for (int i = 1; i < 100; i++) {
+	for (int i = 2; i < 100; i++) { //2 is the only even prime number, try to only count with odds to make it more efficient
 		
 		if (isPrime(i))
 			cout << i << endl;
